fd9fb82877835cd1d5c39a62c8c8fdf1d43cbe59
Dragon11150221 <58123064+Dragon11150221@users.noreply.github.com>
2021-03-01 14:04:41 +0800

    connect to database rebuild project

--- Project007/app/src/main/java/com/example/project007/MainActivity.java
+++ Project007/app/src/main/java/com/example/project007/MainActivity.java
@@ -4,11 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        db = FirebaseFirestore.getInstance();
+        final CollectionReference collectionExperiment = db.collection("Experiments");
+        final CollectionReference collectionUser = db.collection("Experiments");
+
     }
 }
