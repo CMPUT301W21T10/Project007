e38a964f90ae1093d37ce6d0271c56d43ae801a9
Dragon11150221 <58123064+Dragon11150221@users.noreply.github.com>
2021-03-02 12:37:47 +0800

    basic code

---
+++ Project007/app/src/main/java/com/example/project007/Experiment.java
@@ -0,0 +1,22 @@
+package com.example.project007;
+
+public class Experiment {
+
+    private String experimentName;
+    private String experimentDescription;
+    private String experimentDate;
+    // type {1: counts, 2: binomial trials, 3: non-negative integer counts, 4: measurement trials}
+    private Integer type;
+    private Integer id;
+    private Integer owner;
+
+    private Integer Counts;
+    private boolean[] binomialTrails;
+    private Integer nonNegativeCounts;
+    private Integer[] measurementTrails;
+
+
+
+
+
+}
