34fb7780bbcecc389d3e18dc966f4849d2f34daf
Haoyuan Li <haoyuan8@ualberta.ca>
2021-03-19 18:12:02 +0800

    qrcode

---
+++ code/app/src/main/java/com/example/project007/QrcodeFragment.java
@@ -0,0 +1,76 @@
+package com.example.project007;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class QrcodeFragment extends Fragment {
+
+    static QrcodeFragment newInstance(Trails trails){
+        Bundle args = new Bundle();
+        args.putSerializable("result", trails);
+        QrcodeFragment fragment = new QrcodeFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    /**
+     * 生成固定大小的二维码(不需网络权限)
+     *
+     * @param content 需要生成的内容
+     * @param width   二维码宽度
+     * @param height  二维码高度
+     * @return
+     */
+    private Bitmap generateBitmap(String content, int width, int height) {
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        Map<EncodeHintType, String> hints = new HashMap<>();
+        hints.put(EncodeHintType.CHARACTER_SET, "utf-8");
+        try {
+            BitMatrix encode = qrCodeWriter.encode(content, BarcodeFormat.QR_CODE, width, height, hints);
+            int[] pixels = new int[width * height];
+            for (int i = 0; i < height; i++) {
+                for (int j = 0; j < width; j++) {
+                    if (encode.get(j, i)) {
+                        pixels[i * width + j] = 0x00000000;
+                    } else {
+                        pixels[i * width + j] = 0xffffffff;
+                    }
+                }
+            }
+            return Bitmap.createBitmap(pixels, 0, width, width, height, Bitmap.Config.RGB_565);
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view =inflater.inflate(R.layout.fragment_qrcode, container, false);
+        ImageView imageView = view.findViewById(R.id.image_zxing);
+        String word="hello";
+        imageView.setImageBitmap(generateBitmap(word,250,250));
+        return view;
+
+    }
+
+}
