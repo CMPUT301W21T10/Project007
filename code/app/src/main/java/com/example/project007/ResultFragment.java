d352fc0f6319b9c12e9ec3925929dd8618e1b11e
solitariaa <66712118+solitariaa@users.noreply.github.com>
2021-03-23 11:09:55 -0600

    Update

--- code/app/src/main/java/com/example/project007/ResultFragment.java
+++ code/app/src/main/java/com/example/project007/ResultFragment.java
@@ -35,9 +35,12 @@ import lecho.lib.hellocharts.util.ChartUtils;
 import lecho.lib.hellocharts.view.ColumnChartView;
 import lecho.lib.hellocharts.view.LineChartView;
 
-import static android.graphics.Insets.add;
-import static com.example.project007.R2.id.add;
-
+/**
+ * This class show the result of the trails, which includes six numbers and two plots.
+ * The six numbers are the three quartile numbers and median and mean.
+ * The two plots are the histogram and the linear chart run with the time.
+ * @return view
+ */
 
 public class ResultFragment extends Fragment{
     private String type;
@@ -71,7 +74,8 @@ public class ResultFragment extends Fragment{
 
 
     //   四种数据的计算
-    public double[] Quartiles(double[] val) {
+
+    private double[] Quartiles(double[] val) {
         double[] ans = new double[3];
 
         for (int quartileType = 1; quartileType < 4; quartileType++) {
@@ -89,7 +93,7 @@ public class ResultFragment extends Fragment{
         }
         return ans;
     }
-    public static double Med(ArrayList<Double> total){
+    private static double Med(ArrayList<Double> total){
         double j = 0;
         //集合排序
         Collections.sort(total);
@@ -102,14 +106,14 @@ public class ResultFragment extends Fragment{
         }
         return j;
     }
-    public static double avg(double[] x) {
+    private static double avg(double[] x) {
         double sum = 0;//用来保存数组内所有数值的和
         for (double v : x) {
             sum += v;//遍历每一个数组相，将每一个数组相的值加到sum上
         }
         return sum / x.length;
     }
-    public static double StandardDiviation(double[] x) {
+    private static double StandardDiviation(double[] x) {
         double dAve=avg(x);
         double dVar=0;
         for (double v : x) {//求方差
@@ -117,6 +121,13 @@ public class ResultFragment extends Fragment{
         }
         return Math.sqrt(dVar/x.length);
     }
+    /**
+     * This method create a list to record the data of result.
+     * The data includes six numbers,which are the three quartile numbers and median and mean.
+     * @param{ArrayList<Trails>}argument
+     * @param{TrailsActivity}activity
+     * @return list
+     */
     @RequiresApi(api = Build.VERSION_CODES.N)
     public ArrayList<String> CreateList(ArrayList<Trails> argument,TrailsActivity activity){
         ArrayList<Double> list = new ArrayList<>();
@@ -167,7 +178,7 @@ public class ResultFragment extends Fragment{
 //    准备数据绘制线性图
     //提取ArrayList<Trails>的variesData并转化成double[]
     @RequiresApi(api = Build.VERSION_CODES.N)
-    public double[] getList(ArrayList<Trails> argument,String name){
+    private double[] getList(ArrayList<Trails> argument,String name){
         ArrayList<Double> list = new ArrayList<>();
         int l = 0;
         if(name.equals("Binomial")){
@@ -191,7 +202,7 @@ public class ResultFragment extends Fragment{
         return list.stream().mapToDouble(i->i).toArray();
     }
     //y轴获取最大值
-    public static double getMaxValue(double[] arr) {
+    private static double getMaxValue(double[] arr) {
         double max = arr[0];
         for(int i=0;i<arr.length;i++) {
             if(arr[i]>max)
@@ -307,14 +318,14 @@ public class ResultFragment extends Fragment{
     }
 
 //绘制直方图 准备数据
-    public float getNumberOfSuccess(ArrayList<Trails> argument){
+    private float getNumberOfSuccess(ArrayList<Trails> argument){
         float i = 0;
         for(Trails t : argument){
             i = i + Float.parseFloat(t.getSuccess());
         }
         return i;
     }
-    public float getNumberOfFailure(ArrayList<Trails> argument){
+    private float getNumberOfFailure(ArrayList<Trails> argument){
         float i = 0;
         for(Trails t : argument){
             i = i + Float.parseFloat(t.getFailure());
@@ -323,7 +334,7 @@ public class ResultFragment extends Fragment{
     }
 
     @RequiresApi(api = Build.VERSION_CODES.N)
-    public double[] getNum(ArrayList<Trails> argument){
+    private double[] getNum(ArrayList<Trails> argument){
         ArrayList<Double> list = new ArrayList<>();
         for (Trails t : argument) {
             list.add(Double.parseDouble(t.getVariesData()));
@@ -331,7 +342,7 @@ public class ResultFragment extends Fragment{
         return list.stream().mapToDouble(i->i).toArray();
     }
     @RequiresApi(api = Build.VERSION_CODES.N)
-    public ArrayList<Double> getXaxis(ArrayList<Trails> argument){
+    private ArrayList<Double> getXaxis(ArrayList<Trails> argument){
         double[] d = getNum(argument);
         double max = getMaxValue(d);
         ArrayList<Double> list = new ArrayList<>();
@@ -343,7 +354,7 @@ public class ResultFragment extends Fragment{
         return list;
     }
     @RequiresApi(api = Build.VERSION_CODES.N)
-    public ArrayList<Float> getYaxis(ArrayList<Trails> argument){
+    private ArrayList<Float> getYaxis(ArrayList<Trails> argument){
         ArrayList<Double> list = getXaxis(argument);
         ArrayList<Float> l = new ArrayList<>();
         float[]item={0,0,0,0,0};
@@ -366,7 +377,7 @@ public class ResultFragment extends Fragment{
         }
         return l;
     }
-    public float getMax(ArrayList<Float> arr){
+    private float getMax(ArrayList<Float> arr){
         float max = arr.get(0);
         for(int i=0;i<arr.size();i++) {
             if(arr.get(i)>max)
@@ -451,12 +462,20 @@ public class ResultFragment extends Fragment{
     }
 
     @RequiresApi(api = Build.VERSION_CODES.N)
-    public ArrayList<Trails> sortTime(ArrayList<Trails> arr){
+    private ArrayList<Trails> sortTime(ArrayList<Trails> arr){
         arr.sort((p1, p2) -> p1.getTime().compareTo(p2.getTime()));
 
         return arr;
     }
-
+    /**
+     * This method create a view to show the results and the plots.
+     * The six numbers are the three quartile numbers and median and mean.
+     * The two plots are the histogram and the linear chart run with the time.
+     * @param{LayoutInflater}inflater
+     * @param{ViewGroup}container
+     * @param{Bundle}savedInstanceState
+     * @return view
+     */
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     public View onCreateView(@NotNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
