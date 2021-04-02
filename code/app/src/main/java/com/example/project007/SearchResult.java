b00b4ad2ff34c358f5870975f8cc8a201580207c
Dragon11150221 <58123064+Dragon11150221@users.noreply.github.com>
2021-04-02 18:36:56 +0800

    done subscription

---
+++ code/app/src/main/java/com/example/project007/SearchResult.java
@@ -0,0 +1,135 @@
+package com.example.project007;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.example.project007.ui.subscription.SubscriptionViewModel;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class SearchResult extends AppCompatActivity {
+
+    private SubscriptionViewModel subscriptionViewModel;
+    private ListView experimentList;
+    private ArrayAdapter<Experiment> experimentAdapter;
+    private ArrayList<Experiment> experimentDataList;
+    final String TAG = "Sample";
+    private String searchKey = "";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_result);
+
+        Intent intent = getIntent();
+        searchKey = intent.getStringExtra("Key");
+
+        experimentDataList = new ArrayList<>();
+        experimentAdapter = new ExperimentAdapter(this, experimentDataList);
+        experimentList = findViewById(R.id.subscript_list);
+        experimentList.setAdapter(experimentAdapter);
+
+        final FirebaseFirestore db;
+        db = DatabaseController.getDb();
+        final CollectionReference collectionReference = db.collection("Experiments");
+
+        collectionReference.addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException error) {
+                // Clear the old list
+                if (error!=null){
+                    Log.d(TAG,"Error:"+error.getMessage());
+                }
+                else {
+                    experimentDataList.clear();
+                    for (QueryDocumentSnapshot doc : queryDocumentSnapshots) {
+
+                        Experiment oneExperiment = null;
+                        if (doc.exists()) {
+                            // convert document to POJO
+                            oneExperiment = doc.toObject(Experiment.class);
+                            System.out.println(oneExperiment);
+
+                            if (oneExperiment.getSubscriptionId().contains(DatabaseController.getUserId())){
+                                experimentDataList.add(oneExperiment);
+                            }
+                        } else {
+                            System.out.println("No such document!");
+                        }
+
+                    }
+                    processData();
+                    experimentAdapter.notifyDataSetChanged(); // Notifying the adapter to render any new data fetched
+                }
+            }
+        });
+
+        // listener to access detail of an element
+        // package an experiment and position info in intent
+        experimentList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(SearchResult.this, TrailsActivity.class);
+                Experiment instanceExperiment = experimentDataList.get(position);
+                intent.putExtra("com.example.project007.INSTANCE", instanceExperiment);
+                intent.putExtra("com.example.project007.POSITION", position);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    private void processData(){
+        ArrayList<Experiment> result = new ArrayList<Experiment>();
+        for (int i = 0; i<experimentDataList.size(); i++){
+            Experiment experiment = experimentDataList.get(i);
+
+
+            if (experiment.getName().contains(searchKey)){
+                result.add(experiment);
+                break;
+            }
+            if (experiment.getDescription().contains(searchKey)){
+                result.add(experiment);
+                break;
+            }
+            if (experiment.getRegion().contains(searchKey)){
+                result.add(experiment);
+                break;
+            }
+            if (experiment.getType().contains(searchKey)){
+                result.add(experiment);
+                break;
+            }
+            if (experiment.getDate().contains(searchKey)){
+                result.add(experiment);
+                break;
+            }
+            if (experiment.isCondition() && searchKey.equals("End")){
+                result.add(experiment);
+                break;
+            }
+            if (!experiment.isCondition() && searchKey.equals("Processing")){
+                result.add(experiment);
+                break;
+            }
+
+        }
+        experimentDataList = result;
+
+    }
+}
--- code/app/src/main/java/com/example/project007/ui/home/HomeFragment.java
+++ code/app/src/main/java/com/example/project007/ui/home/HomeFragment.java
@@ -10,6 +10,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -27,6 +30,7 @@ import com.example.project007.ModifyExperimentFragment;
 import com.example.project007.Question;
 import com.example.project007.QuestionDatabaseController;
 import com.example.project007.R;
+import com.example.project007.SearchResult;
 import com.example.project007.TrailsActivity;
 import com.example.project007.UserEntity;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -161,6 +165,29 @@ public class HomeFragment extends Fragment {
             }
         });
 
+        final FloatingActionButton searchButton = root.findViewById(R.id.search_experiment_button);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LinearLayout searchBar = root.findViewById(R.id.search_bar);
+                searchBar.setVisibility(View.VISIBLE);
+            }
+        });
+
+        final Button confirmButton = root.findViewById(R.id.confirm_button);
+        confirmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LinearLayout searchBar = root.findViewById(R.id.search_bar);
+                searchBar.setVisibility(View.INVISIBLE);
+                Intent intent = new Intent(getActivity(), SearchResult.class);
+                EditText searchKey = root.findViewById(R.id.search_key);
+                intent.putExtra("Key", searchKey.getText().toString());
+                searchKey.setText("");
+                startActivity(intent);
+            }
+        });
+
         collectionReference.addSnapshotListener(new EventListener<QuerySnapshot>() {
             @Override
             public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException error) {
@@ -182,6 +209,7 @@ public class HomeFragment extends Fragment {
                             System.out.println("No such document!");
                         }
                     }
+
                     experimentAdapter.notifyDataSetChanged(); // Notifying the adapter to render any new data fetched
                 }
 
