365de28b4f7c44adad0ba18a3593f028561d4110
solitariaa <ziying7@ualberta.ca>
2021-04-06 01:55:17 +0800

    updatedatedatedate

--- code/app/src/androidTest/java/com/example/project007/AppTest.java
+++ code/app/src/androidTest/java/com/example/project007/AppTest.java
@@ -18,6 +18,7 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
+import static java.lang.Thread.sleep;
 import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertFalse;
 import static junit.framework.TestCase.assertTrue;
@@ -38,7 +39,7 @@ public class AppTest {
      *Login into the app.
      */
     @Test
-    public void StartTest() {
+    public void StartTest() throws InterruptedException {
         //测试进入
         solo.assertCurrentActivity("Wrong Activity", LoginActivity.class);
         solo.clickOnText("Sign In");
@@ -48,63 +49,157 @@ public class AppTest {
         solo.clickOnView((FloatingActionButton) solo.getView(R.id.add_experiment_button));
         solo.enterText((EditText) solo.getView(R.id.editTextName), "Count-base");
         solo.enterText((EditText) solo.getView(R.id.editTextDescription), "Count Numbers");
-        solo.pressSpinnerItem(0, 1);
+        solo.pressSpinnerItem(0, 0);
         solo.enterText((EditText) solo.getView(R.id.minimumTrails), "0");
         solo.clickOnText("OK");
         assertTrue(solo.waitForText("Count-base", 1, 2000));
         //查找并进入count-base
         ListView currentListView = (ListView) solo.getView("experiment_list");
-        View convertView =  currentListView.getChildAt(4);
-        solo.clickOnView(convertView);
+        View view1 =  currentListView.getChildAt(0);
+        solo.clickOnView(view1);
         solo.assertCurrentActivity("Wrong Activity", TrailsActivity.class);
-        final TextView textView = (TextView) solo.getView("name_view"); // Get the listview
-        String message = (String) textView.getText().toString(); // Get item from first position
-        assertEquals("Count-base", message);
+        final TextView textView1 = (TextView) solo.getView("name_view"); // Get the listview
+        String message1 = (String) textView1.getText().toString(); // Get item from first position
+        assertEquals("Count-base", message1);
         //添加第一条count-base的trail
         solo.clickOnView((FloatingActionButton) solo.getView(R.id.experimentBtn));
         solo.enterText((EditText) solo.getView(R.id.trail_Title_editText), "Trail1");
         solo.enterText((EditText) solo.getView(R.id.ResultText), "10");
-        solo.clickOnText("OK");
+        solo.getButton("OK");
         //添加第二条count-base的trail
         solo.clickOnView((FloatingActionButton) solo.getView(R.id.experimentBtn));
         solo.enterText((EditText) solo.getView(R.id.trail_Title_editText), "Trail2");
         solo.enterText((EditText) solo.getView(R.id.ResultText), "20");
-        solo.clickOnText("OK");
+        solo.getButton("OK");
         //添加第三条count-base的trail
         solo.clickOnView((FloatingActionButton) solo.getView(R.id.experimentBtn));
         solo.enterText((EditText) solo.getView(R.id.trail_Title_editText), "Trail2");
         solo.enterText((EditText) solo.getView(R.id.ResultText), "30");
-        solo.clickOnText("OK");
+        solo.goBack();
+        solo.getButton("OK");
+        //测试results
+        solo.clickOnView(solo.getView(R.menu.menu_trails_activity),false);
+        solo.clickOnButton("View Result(Plots)");
+        sleep(3000);
+        solo.scrollToBottom();
+        sleep(3000);
+        solo.goBack();
+        solo.clickOnButton("View All Location?");
+        sleep(3000);
+        solo.goBack();
+        solo.clickOnButton("Help/Tips");
         solo.goBack();
 
-//        //添加count-base
-//        solo.clickOnView((FloatingActionButton) solo.getView(R.id.add_experiment_button));
-//        solo.enterText((EditText) solo.getView(R.id.editTextName), "Count-base");
-//        solo.enterText((EditText) solo.getView(R.id.editTextDescription), "Count Numbers");
-//        solo.pressSpinnerItem(0, 1);
-//        solo.enterText((EditText) solo.getView(R.id.minimumTrails), "0");
-//        solo.clickOnText("OK");
-//        assertTrue(solo.waitForText("Count-base", 1, 2000));
-//
-//        //查找并进入count-base
-//        ListView currentListView = (ListView) solo.getView("experiment_list");
-//        View convertView =  currentListView.getChildAt(0);
-//        solo.clickOnView(convertView);
-//        solo.assertCurrentActivity("Wrong Activity", TrailsActivity.class);
-//        final TextView textView = (TextView) solo.getView("name_view"); // Get the listview
-//        String message = (String) textView.getText().toString(); // Get item from first position
-//        assertEquals("Count-base", message);
-//
-//        //添加第一条count-base的trail
-//        solo.clickOnView((FloatingActionButton) solo.getView(R.id.experimentBtn));
-//        solo.enterText((EditText) solo.getView(R.id.trail_Title_editText), "Trail1");
-//        solo.enterText((EditText) solo.getView(R.id.ResultText), "12");
-//        solo.clickOnText("OK");
+        //添加Binomial
+        solo.clickOnView((FloatingActionButton) solo.getView(R.id.add_experiment_button));
+        solo.enterText((EditText) solo.getView(R.id.editTextName), "Binomial");
+        solo.enterText((EditText) solo.getView(R.id.editTextDescription), "TrueOrFalse");
+        solo.pressSpinnerItem(0, 1);
+        solo.enterText((EditText) solo.getView(R.id.minimumTrails), "0");
+        solo.clickOnText("OK");
+        assertTrue(solo.waitForText("Binomial", 1, 2000));
+        //查找并进入Binomial
+        View view2 =  currentListView.getChildAt(1);
+        solo.clickOnView(view2);
+        solo.assertCurrentActivity("Wrong Activity", TrailsActivity.class);
+        final TextView textView2 = (TextView) solo.getView("name_view"); // Get the listview
+        String message2 = (String) textView2.getText().toString(); // Get item from first position
+        assertEquals("Binomial", message2);
+        //添加第一条Binomial的trail
+        solo.clickOnView((FloatingActionButton) solo.getView(R.id.experimentBtn));
+        solo.enterText((EditText) solo.getView(R.id.trail_Title_editText), "Trail1");
+        solo.enterText((EditText) solo.getView(R.id.SuccessText), "10");
+        solo.enterText((EditText) solo.getView(R.id.failText), "10");
+        //添加第二条Binomial的trail
+        solo.clickOnView((FloatingActionButton) solo.getView(R.id.experimentBtn));
+        solo.enterText((EditText) solo.getView(R.id.trail_Title_editText), "Trail1");
+        solo.enterText((EditText) solo.getView(R.id.SuccessText), "20");
+        solo.enterText((EditText) solo.getView(R.id.failText), "10");
+        //添加第三条Binomial的trail
+        solo.clickOnView((FloatingActionButton) solo.getView(R.id.experimentBtn));
+        solo.enterText((EditText) solo.getView(R.id.trail_Title_editText), "Trail1");
+        solo.enterText((EditText) solo.getView(R.id.SuccessText), "30");
+        solo.enterText((EditText) solo.getView(R.id.failText), "10");
+        //测试results
+        solo.clickOnView(solo.getView(R.menu.menu_trails_activity),false);
+        solo.clickOnButton("View Result(Plots)");
+        sleep(3000);
+        solo.scrollToBottom();
+        sleep(3000);
+        solo.goBack();
 
+        //添加Non-negative
+        solo.clickOnView((FloatingActionButton) solo.getView(R.id.add_experiment_button));
+        solo.enterText((EditText) solo.getView(R.id.editTextName), "Non-negative");
+        solo.enterText((EditText) solo.getView(R.id.editTextDescription), "Non-integer numbers");
+        solo.pressSpinnerItem(0, 2);
+        solo.enterText((EditText) solo.getView(R.id.minimumTrails), "0");
+        solo.clickOnText("OK");
+        assertTrue(solo.waitForText("Binomial", 1, 2000));
+        //查找并进入Non-negative
+        View view3 =  currentListView.getChildAt(2);
+        solo.clickOnView(view3);
+        solo.assertCurrentActivity("Wrong Activity", TrailsActivity.class);
+        final TextView textView3 = (TextView) solo.getView("name_view"); // Get the listview
+        String message3 = (String) textView3.getText().toString(); // Get item from first position
+        assertEquals("Binomial", message3);
+        //添加第一条Non-negative的trail
+        solo.clickOnView((FloatingActionButton) solo.getView(R.id.experimentBtn));
+        solo.enterText((EditText) solo.getView(R.id.trail_Title_editText), "Trail1");
+        solo.enterText((EditText) solo.getView(R.id.ResultText), "10.0");
+        //添加第二条Non-negative的trail
+        solo.clickOnView((FloatingActionButton) solo.getView(R.id.experimentBtn));
+        solo.enterText((EditText) solo.getView(R.id.trail_Title_editText), "Trail1");
+        solo.enterText((EditText) solo.getView(R.id.ResultText), "20");
+        //添加第三条Non-negative的trail
+        solo.clickOnView((FloatingActionButton) solo.getView(R.id.experimentBtn));
+        solo.enterText((EditText) solo.getView(R.id.trail_Title_editText), "Trail1");
+        solo.enterText((EditText) solo.getView(R.id.ResultText), "30.0");
+        //测试results
+        solo.clickOnView(solo.getView(R.menu.menu_trails_activity),false);
+        solo.clickOnButton("View Result(Plots)");
+        sleep(3000);
+        solo.scrollToBottom();
+        sleep(3000);
+        solo.goBack();
 
+        //添加Measurement
+        solo.clickOnView((FloatingActionButton) solo.getView(R.id.add_experiment_button));
+        solo.enterText((EditText) solo.getView(R.id.editTextName), "Measurement");
+        solo.enterText((EditText) solo.getView(R.id.editTextDescription), "Floating numbers");
+        solo.pressSpinnerItem(0, 3);
+        solo.enterText((EditText) solo.getView(R.id.minimumTrails), "0");
+        solo.clickOnText("OK");
+        assertTrue(solo.waitForText("Measurement", 1, 2000));
+        //查找并进入Measurement
+        View view4 =  currentListView.getChildAt(3);
+        solo.clickOnView(view4);
+        solo.assertCurrentActivity("Wrong Activity", TrailsActivity.class);
+        final TextView textView4 = (TextView) solo.getView("name_view"); // Get the listview
+        String message4 = (String) textView4.getText().toString(); // Get item from first position
+        assertEquals("Binomial", message4);
+        //添加第一条Measurement的trail
+        solo.clickOnView((FloatingActionButton) solo.getView(R.id.experimentBtn));
+        solo.enterText((EditText) solo.getView(R.id.trail_Title_editText), "Trail1");
+        solo.enterText((EditText) solo.getView(R.id.ResultText), "10.0");
+        //添加第二条Measurement的trail
+        solo.clickOnView((FloatingActionButton) solo.getView(R.id.experimentBtn));
+        solo.enterText((EditText) solo.getView(R.id.trail_Title_editText), "Trail1");
+        solo.enterText((EditText) solo.getView(R.id.ResultText), "20.0");
+        //添加第三条Measurement的trail
+        solo.clickOnView((FloatingActionButton) solo.getView(R.id.experimentBtn));
+        solo.enterText((EditText) solo.getView(R.id.trail_Title_editText), "Trail1");
+        solo.enterText((EditText) solo.getView(R.id.ResultText), "30.0");
+        //测试results
+        solo.clickOnView(solo.getView(R.menu.menu_trails_activity),false);
+        solo.clickOnButton("View Result(Plots)");
+        sleep(3000);
+        solo.scrollToBottom();
+        sleep(3000);
+        solo.goBack();
 
-
-//        solo.clickLongOnView(convertView);
+        //测试长按的
+        solo.clickLongOnView(view1);
 //
 //        //Testing Trails frags
 //        solo.clickOnView((FloatingActionButton) solo.getView(R.id.experimentBtn));
